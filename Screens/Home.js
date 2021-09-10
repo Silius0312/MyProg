9d74122a61e35a2f449a643b2b98cfb4470c647b
Silius0312 <silius0312@gmail.com>
2021-09-10 22:47:53 +0300

    Стишок

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Button } from "react-native";
+import { View, Button, Text } from "react-native";
 
 
 export const Home = (props) => {
@@ -12,6 +12,11 @@ export const Home = (props) => {
                 navigation.navigate('Screen1') 
              } 
         />
+        <Text> Как же долго я ебался,{"\n"}
+            Наконец-то докапался,{"\n"}
+            Я ошибки всё искал,{"\n"}
+            Влад мне в этом помогал.
+        </Text>
         </View>     
     );
 };
--- Screens/Screen1.js
+++ Screens/Screen1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, View } from "react-native";
+import { Button, View, Text } from "react-native";
 
 
 export const Screen1 = ({navigation}) => {
@@ -11,6 +11,11 @@ export const Screen1 = ({navigation}) => {
             navigation.navigate('Home') 
     } 
     /> 
+    <Text> А теперь всё заепца,{"\n"}
+        Счастье полная штанца,{"\n"}
+        Во какой я Молодец,{"\n"}
+        Скоро будет всем 3.14*дец...{"\n"}
+    </Text>
         </View>  
     );
 };
