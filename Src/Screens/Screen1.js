a02bcba1ff237c46df8f5cd319a2632a577d5a1f
Silius0312 <silius0312@gmail.com>
2021-09-11 22:31:51 +0300

    По зАпросу Сэн-Сэя....

--- Src/Screens/Screen1.js
+++ Src/Screens/Screen1.js
@@ -2,33 +2,9 @@ import React from 'react';
 import { Button, View, Text } from "react-native";
 
 
-export const Screen1 = ({navigation}) => {
+export const Screen1 = () => {
     return (
-        <View>            
-            <Button 
-            title =  "Главная"
-            onPress={() => 
-            navigation.navigate('Home') 
-    } 
-    /> 
-            <Button 
-                title =  "Страница 1"
-                onPress={() => 
-                navigation.navigate('Screen1') 
-             } 
-        />
-            <Button 
-            title = "Страница 2" 
-            onPress ={() =>
-            navigation.navigate('Screen2')
-            }
-            /> 
-            <Button 
-            title = "Страница 3" 
-            onPress ={() =>
-            navigation.navigate('HScreen3')
-            }
-            /> 
+        <View>          
     <Text>1 страница</Text>
         </View>  
     );
--- Src/Screens/Screen2.js
+++ Src/Screens/Screen2.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
 
-export const Screen2 = ({navigation}) => {
+export const Screen2 = () => {
     return (
-        <View>
-            <Button 
-            title = "Главная" 
-            onPress ={() =>
-            navigation.navigate('Home')
-            }
-            /> 
-            <Button 
-            title = "Страница 1" 
-            onPress ={() =>
-            navigation.navigate('Screen1')
-            }
-            /> 
-             <Button 
-            title = "Страница 2" 
-            onPress ={() =>
-            navigation.navigate('Screen2')
-            }
-            /> 
-            <Button 
-            title = "Страница 3" 
-            onPress ={() =>
-            navigation.navigate('Screen3')
-            }
-            /> 
+        <View>           
             <Text>2 страница</Text>
         </View>
     )
