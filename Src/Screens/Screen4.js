693604bd2f19e9d7853385b4ce4b18a7381143b8
Silius0312 <silius0312@gmail.com>
2021-09-22 21:59:20 +0300

    перед redux

--- Src/Screens/Screen4.js
+++ Src/Screens/Screen4.js
@@ -2,19 +2,7 @@ import React from "react";
 import { Button, View, Text } from "react-native";
 
 
-export const Screen4 = ({navigation, route}) => { 
-    const txtFrom5 = route.params.text
-    const passFrom5 = route.params.number
-    return (
-        <View>     
-            <Button
-                title="Войти"
-                onPress={() => {
-                navigation.navigate('Screen5')
-        }}
-        />                      
-                <Text>{txtFrom5}</Text>
-                <Text>{passFrom5}</Text>                 
-         </View>       
-    );   
-};
+export const Screen4 = () => {
+    return 
+}
+    
