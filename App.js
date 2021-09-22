e9d48c0822a847421c6e39782b71fb3baf54c15a
Silius0312 <silius0312@gmail.com>
2021-09-22 22:16:12 +0300

    123

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import { Navigate } from './Src/navigation/navigations';
 
 
 const App = () => {
     return ( 
-       
+       <Provider>
       <Navigate/>
+      </Provider>
           
   );
 };
---
+++ Src/redux/reducer/store.js
@@ -0,0 +1,11 @@
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+
+
+
+const allReducers = combineReducers ({
+    product: productList,
+});
+
+
+export const store  = createStore(allReducers);
