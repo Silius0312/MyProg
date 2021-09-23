a65f8e055737153ed41f86c00b51cf9ce49ba07c
Silius0312 <silius0312@gmail.com>
2021-09-23 14:43:30 +0300

    Clear Project and add First Reducer

--- Src/Screens/Home.js
+++ Src/Screens/Home.js
@@ -1,42 +1,12 @@
 import React from 'react';
-import { View, Button, Text } from "react-native";
+import { View, Text } from "react-native";
 
 
 export const Home = (props) => {
     const navigation = props.navigation;
     return (                   
         <View>
-            <Text>Главная Страница</Text>
-              <Button 
-            title =  "Главная"
-            onPress={() => 
-            navigation.navigate('Home') 
-    } 
-    /> 
-            <Button 
-                title =  "Страница 1"
-                onPress={() => 
-                navigation.navigate('Screen1') 
-             } 
-        />
-         <Button 
-                title =  "Страница 2"
-                onPress={() => 
-                navigation.navigate('Screen2') 
-             } 
-        />
-         <Button 
-                title =  "Страница 3"
-                onPress={() => 
-                navigation.navigate('Screen3') 
-             } 
-        />
-        <Button 
-                title =  "Страница 4"
-                onPress={() => 
-                navigation.navigate('Screen4') 
-             } 
-        />
+            <Text>Hello!</Text>
              </View>     
     );
 };
