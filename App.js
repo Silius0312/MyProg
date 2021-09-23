2101d13ce287318c81b1f381b14431dd6febbe1f
Silius0312 <silius0312@gmail.com>
2021-09-23 14:33:14 +0300

    hello

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 
 import { Navigate } from './Src/navigation/navigations';
 
 
 const App = () => {
     return ( 
-       <Provider>
       <Navigate/>
-      </Provider>
           
   );
 };
