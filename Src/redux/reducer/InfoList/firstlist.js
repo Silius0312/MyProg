a10ccd19f1c5a964f77140d839bf6fc6ab1b70e1
Silius0312 <silius0312@gmail.com>
2021-09-28 08:19:07 +0300

    reducer

---
+++ Src/redux/reducer/InfoList/firstlist.js
@@ -0,0 +1,12 @@
+export const FirstR = () => {
+    return    [
+       {
+           id:1,
+           title:"Тренировки"
+       },
+       {
+           id:2,
+           title:"Прогресс"
+       },
+           ]
+   };
