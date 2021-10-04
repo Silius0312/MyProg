9604c77c4e5130f079b2512bec50911d42c8510e
Silius0312 <silius0312@gmail.com>
2021-10-04 12:41:53 +0300

    Навигация

--- Src/Screens/Home.js
+++ Src/Screens/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { SafeAreaView, Text } from "react-native";
 import {First} from "../components/first"
-import { Workout } from '../components/Workout';
+
 
 
 export const Home = () => {
         return (                   
         <SafeAreaView>
-            <First />
-            <Workout/>
+            <First />           
              </SafeAreaView>     
     );
 };
