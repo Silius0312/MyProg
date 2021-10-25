c5fc00b211df7baa76232af0727d661113166dbf
Silius0312 <silius0312@gmail.com>
2021-10-25 15:28:44 +0300

    route

--- Src/redux/reducer/InfoList/worklist.js
+++ Src/redux/reducer/InfoList/worklist.js
@@ -1,23 +1,34 @@
 export const WorkList = [
          {
             id:1,
-            title:'Жим лёжа'
+            title:'Жим лёжа',
+            approach: 5,
+            weight: 60
+            
           },
         {
             id:2,
-            title:'Подтягивание'
+            title:'Подтягивание',
+            approach: 5,
+            weight: 75
         },
          {
             id:3,
-            title:'Приседание со штангой'            
+            title:'Приседание со штангой',
+            approach: 5, 
+            weight: 50           
          },
         {   
             id:4,
-            title:'Жим на плечи'
+            title:'Жим на плечи',
+            approach: 5,
+            weight: 25
         },
         {
             id:5,
-            title: 'Пресс'
+            title: 'Пресс',
+            approach: 5,
+            weight: 75
         },
 
         ]
