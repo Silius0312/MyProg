b420eee6bd7c68adc0210490c7ea3523bacfa231
Silius0312 <silius0312@gmail.com>
2021-10-26 22:06:01 +0300

    плюс минус

--- Src/components/AddWorks.js
+++ Src/components/AddWorks.js
@@ -1,62 +1,62 @@
-import React, {useState} from 'react';
-import { Button, SafeAreaView, StyleSheet, TextInput } from 'react-native';
+// import React, {useState} from 'react';
+// import { Button, SafeAreaView, StyleSheet, TextInput } from 'react-native';
 
-import { useDispatch } from 'react-redux';
-import { addNewWorkout } from '../redux/reducer/reducer';
+// import { useDispatch } from 'react-redux';
+// import { addNewWorkout } from '../redux/reducer/reducer';
 
-export  const AddWorks = ({ navigation: { goBack }}) => {  
+// export  const AddWorks = ({ navigation: { goBack }}) => {  
         
 
-        const [name, setName]= useState(null);  
-        const [weight, setWeight] = useState(null);
-        const [amount, setAmount] = useState(null);
+//         const [name, setName]= useState(null);  
+//         const [weight, setWeight] = useState(null);
+//         const [amount, setAmount] = useState(null);
 
-        const onChangeName = (text) => {setName(text)};
-        const onChangeWeigt = (number) => {setWeight(number)};
-        const onChangeAmount = (number) => {setAmount(number)};
+//         const onChangeName = (text) => {setName(text)};
+//         const onChangeWeigt = (number) => {setWeight(number)};
+//         const onChangeAmount = (number) => {setAmount(number)};
 
-        function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-        };
-        const newWork = {id: getRandomInt(999999999999), title: name, approach: amount, weight: weight};
-        const dispath = useDispatch();
-        return (
-        <SafeAreaView>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeName}
-          value={name}
-          placeholder="Название упражнения"         
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeWeigt}
-          value={weight}
-          placeholder="Вес"
-          keyboardType="numeric"
-        />
-          <TextInput
-          style={styles.input}
-          onChangeText={onChangeAmount}
-          value={amount}
-          placeholder="Количество"
-          keyboardType="numeric"
-        />
-        <Button 
-        title = "Добавить упражнение"
-        onPress = {() => {dispath(addNewWorkout(newWork)),
-        goBack();}}      
+//         function getRandomInt(max) {
+//         return Math.floor(Math.random() * max);
+//         };
+//         const newWork = {id: getRandomInt(999999999999), title: name, approach: amount, weight: weight};
+//         const dispath = useDispatch();
+//         return (
+//         <SafeAreaView>
+//         <TextInput
+//           style={styles.input}
+//           onChangeText={onChangeName}
+//           value={name}
+//           placeholder="Название упражнения"         
+//         />
+//         <TextInput
+//           style={styles.input}
+//           onChangeText={onChangeWeigt}
+//           value={weight}
+//           placeholder="Вес"
+//           keyboardType="numeric"
+//         />
+//           <TextInput
+//           style={styles.input}
+//           onChangeText={onChangeAmount}
+//           value={amount}
+//           placeholder="Количество"
+//           keyboardType="numeric"
+//         />
+//         <Button 
+//         title = "Добавить упражнение"
+//         onPress = {() => {dispath(addNewWorkout(newWork)),
+//         goBack();}}      
         
-        />
-      </SafeAreaView>
-    );     
-};
+//         />
+//       </SafeAreaView>
+//     );     
+// };
 
-const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    }
-  });
+// const styles = StyleSheet.create({
+//     input: {
+//       height: 40,
+//       margin: 12,
+//       borderWidth: 1,
+//       padding: 10,
+//     }
+//   });
--- Src/redux/reducer/InfoList/worklist.js
+++ Src/redux/reducer/InfoList/worklist.js
@@ -1,35 +1,8 @@
 export const WorkList = [
-         {
-            id:1,
-            title:'Жим лёжа',
-            approach: 5,
-            weight: 60
-            
-          },
-        {
-            id:2,
-            title:'Подтягивание',
-            approach: 5,
-            weight: 75
-        },
-         {
-            id:3,
-            title:'Приседание со штангой',
-            approach: 5, 
-            weight: 50           
-         },
-        {   
-            id:4,
-            title:'Жим на плечи',
-            approach: 5,
-            weight: 25
-        },
-        {
-            id:5,
-            title: 'Пресс',
-            approach: 5,
-            weight: 73
-        },
-
+        { id:1, title:'Жим лёжа', details: {podhod: {amount:10, weight: 60 }}},
+        { id:2, title:'Подтягивание', details: {podhod:{amount:10, weight: 75 }}},
+        { id:3, title:'Приседание со штангой', podhod: {treatment: {amount: 5, weight: 50 }}},
+        { id:4, title:'Жим на плечи', details: { podhod: {amount: 5, weight: 25 }}},
+        { id:5, title: 'Пресс', details: { podhod:{amount: 5, weight: 73 }}},
         ]
     
